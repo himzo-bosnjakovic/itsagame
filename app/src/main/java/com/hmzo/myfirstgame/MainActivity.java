c069275ff107061b34e1bec6d691728006aca0cb
Himzo Bosnjakovic <himzo.bosnjakovic.2012@gmail.com>
2019-07-10 23:34:15 +0200

    Initial commit

---
+++ app/src/main/java/com/hmzo/myfirstgame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hmzo.myfirstgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
